24ce240baa867c08ca70ed86368b42dc2ccf6b3f
Zywl <lifeerwz@gmail.com>
2025-06-19 22:11:10 -0300

    feat: small fixes x2 favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type React from "react"
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
-  title: 'QrCode',
-  description: 'by Lucas Lima',
+  title: "QrCode",
+  description: "by Lucas Lima",
   icons: {
-    icon: "/favicon.png",
-    shortcut: "/favicon.png",
-    apple: "/favicon.png",
+    icon: "/favicon.svg",
+      shortcut: "/favicon.png",
+      apple: "/favicon.png",
   },
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br">
+      <html lang="pt-br">
       <body>{children}</body>
-    </html>
+      </html>
   )
 }
